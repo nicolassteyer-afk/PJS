a456a903750ed68c6075c1f7848f824d113a41f2
nicolassteyer-afk <nicolassteyer1@gmail.com>
2026-05-25 16:15:21 +0200

    Add grid overlay component

---
+++ src/components/GridOverlay.tsx
@@ -0,0 +1,11 @@
+export default function GridOverlay() {
+  return (
+    <div className="grid-overlay" aria-hidden="true">
+      <span />
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+}
