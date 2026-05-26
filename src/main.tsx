30ad3f1b625fd8ac254f1a891185aa6e4ce0f5cb
nicolassteyer-afk <nicolassteyer1@gmail.com>
2026-05-26 09:33:56 +0200

    Load interaction style layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/global.scss";
+import "./styles/interactive.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
